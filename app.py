d563caf39747960bc1af1eaa025656b3c8265837
Raychura-Meet <158803344+Raychura-Meet@users.noreply.github.com>
2024-04-09 00:10:37 +0530

    apP.py
    Change APi Key

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import openai
 # Initialize your OpenAI API key
-openai.api_key = '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'
+openai.api_key = 'Enter Your Api Key'
 
 # Example Ayurvedic text. Replace this with your actual text.
 ayurvedic_text = """
